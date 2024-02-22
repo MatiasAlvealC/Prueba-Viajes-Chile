aa2e85f7c0b4a8057c0c87857b8603cd50ec6b52
Matias Alveal <matiasalvealc@gmail.com>
2024-02-22 20:18:56 -0300

    Agreado header

---
+++ assets/js/carr.js
@@ -0,0 +1,47 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+const slides = document.querySelectorAll('.carousel-slide img');
+
+let counter = 0;
+const intervalTime = 5000; // Cambia este valor según el tiempo de intervalo en milisegundos
+let slideInterval;
+
+function nextSlide() {
+  slides[counter].style.display = 'none';
+  counter = (counter + 1) % slides.length;
+  slides[counter].style.display = 'block';
+}
+
+
+function prevSlide() {
+  slides[counter].style.display = 'none';
+  counter = (counter - 1 + slides.length) % slides.length;
+  slides[counter].style.display = 'block';
+}
+
+nextBtn.addEventListener('click', () => {
+  nextSlide();
+});
+
+prevBtn.addEventListener('click', () => {
+  prevSlide();
+});
+
+function startSlide() {
+  slideInterval = setInterval(() => {
+    nextSlide();
+  }, intervalTime);
+}
+
+carouselSlide.addEventListener('mouseenter', () => {
+  clearInterval(slideInterval);
+});
+
+carouselSlide.addEventListener('mouseleave', () => {
+  startSlide();
+});
+
+startSlide();
+
